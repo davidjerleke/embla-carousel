6c15c6c21c45daeb7e7159a5ee5bd87dbeb794c2
davidcetinkaya <david.o.cetinkaya@gmail.com>
2019-11-20 18:57:07 +0100

    Rename function and variable

--- src/components/scrollSnap.ts
+++ src/components/scrollSnap.ts
@@ -14,15 +14,15 @@ export type ScrollSnap = {
 
 export function ScrollSnap(params: Params): ScrollSnap {
   const { snapSizes, alignSize, loop } = params
-  const counterLimit = Limit({ min: 0, max: snapSizes.length - 1 })
-  const counter = Counter({ limit: counterLimit, start: 0, loop })
+  const limit = Limit({ min: 0, max: snapSizes.length - 1 })
+  const counter = Counter({ limit, start: 0, loop })
   const alignSizes = snapSizes.map(alignSize.measure)
-  const betweenDistances = distancesBetweenSnaps()
+  const betweenDistances = distancesBetweenScrollSnaps()
 
-  function distancesBetweenSnaps(): number[] {
-    return snapSizes.map((size, i) => {
-      const next = counter.clone().set(i + 1)
-      return size + alignSizes[i] - alignSizes[next.get()]
+  function distancesBetweenScrollSnaps(): number[] {
+    return snapSizes.map((size, index) => {
+      const next = counter.clone().set(index + 1)
+      return size + alignSizes[index] - alignSizes[next.get()]
     })
   }
 
