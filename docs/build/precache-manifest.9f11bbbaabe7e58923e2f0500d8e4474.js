ff3498a832d832c63b122f7ab9e3c0911e38d313
davidcetinkaya <david.o.cetinkaya@gmail.com>
2020-07-12 11:46:45 +0200

    Add 2 links to codesandboxes in docs

---
+++ docs/build/precache-manifest.9f11bbbaabe7e58923e2f0500d8e4474.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeffe9e37fe2efb2bfc112bbcf3255ad",
+    "url": "./build/index.html"
+  },
+  {
+    "revision": "34b8ed583f0fa075ccab",
+    "url": "./build/static/js/2.0ddb850f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./build/static/js/2.0ddb850f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff81c62eb678cf62ffca",
+    "url": "./build/static/js/main.448c1b28.chunk.js"
+  },
+  {
+    "revision": "fed0e258b3fd949477df",
+    "url": "./build/static/js/runtime-main.318cd8e5.js"
+  }
+]);
--- docs/build/service-worker.js
+++ docs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./build/precache-manifest.ceb6144ba32cfb2d8f131d83eaf3e716.js"
+  "./build/precache-manifest.9f11bbbaabe7e58923e2f0500d8e4474.js"
 );
 
 self.addEventListener('message', (event) => {
