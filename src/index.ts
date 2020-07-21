a244a47e493e8443b0e7e181bbd78800e91ce90f
davidcetinkaya <david.o.cetinkaya@gmail.com>
2020-07-21 20:24:11 +0200

    Add Type suffix to all exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+export { EmblaOptions as EmblaOptionsType } from './vanilla/components/options'
+export { EmblaEvent as EmblaEventType } from './vanilla/components/eventEmitter'
+export { EmblaCarousel as EmblaCarouselType } from './vanilla'
+
 export { default } from './vanilla'
 export { useEmblaCarousel } from './react'
-export { EmblaOptions } from './vanilla/components/options'
-export { EmblaEvent } from './vanilla/components/eventEmitter'
