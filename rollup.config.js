cc9b618a6fcfd4adc3057b9dcd0e9e7180ef680e
davidcetinkaya <david.o.cetinkaya@gmail.com>
2021-03-19 21:23:56 +0100

    Enable prettier for rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -5,71 +5,68 @@ import { terser } from 'rollup-plugin-terser'
 import localTypescript from 'typescript'
 import packageJson from './package.json'
 
-const babelConfig = {
+const CONFIG_BABEL = {
   extensions: ['.js', '.jsx', '.ts', '.tsx'],
   exclude: 'node_modules/**',
   babelHelpers: 'bundled',
 }
-const typescriptConfig = {
+const CONFIG_TYPESCRIPT = {
   typescript: localTypescript,
   declaration: true,
   useTsconfigDeclarationDir: true,
 }
 
-export default [
-  {
-    input: 'src/vanilla/index.ts',
-    output: [
-      {
-        file: `${packageJson.name}.js`,
-        format: 'cjs',
-        strict: true,
-        sourcemap: true,
-      },
-      {
-        file: `${packageJson.name}.esm.js`,
-        format: 'esm',
-        strict: true,
-        sourcemap: true,
-      },
-      {
-        format: 'umd',
-        name: 'EmblaCarousel',
-        file: `${packageJson.name}.umd.js`,
-        strict: true,
-        sourcemap: false,
-        plugins: [terser()],
-      },
-    ],
-    plugins: [
-      resolve(),
-      typescript(typescriptConfig),
-      babel(babelConfig),
-    ],
-  },
-  {
-    input: 'src/react/index.ts',
-    output: [
-      {
-        file: 'react.js',
-        format: 'cjs',
-        globals: { react: 'React' },
-        strict: true,
-        sourcemap: true,
-      },
-      {
-        file: 'react.esm.js',
-        format: 'esm',
-        globals: { react: 'React' },
-        strict: true,
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      resolve(),
-      typescript(typescriptConfig),
-      babel(babelConfig),
-    ],
-    external: ['react'],
-  },
-]
+const PACKAGE_VANILLA = (process.env.BUILD !== 'development' ||
+  process.env.PACKAGE === 'vanilla') && {
+  input: 'src/vanilla/index.ts',
+  output: [
+    {
+      file: `${packageJson.name}.js`,
+      format: 'cjs',
+      strict: true,
+      sourcemap: true,
+      exports: 'auto',
+    },
+    {
+      file: `${packageJson.name}.esm.js`,
+      format: 'esm',
+      strict: true,
+      sourcemap: true,
+    },
+    {
+      format: 'umd',
+      name: 'EmblaCarousel',
+      file: `${packageJson.name}.umd.js`,
+      strict: true,
+      sourcemap: false,
+      plugins: [terser()],
+    },
+  ],
+  plugins: [resolve(), typescript(CONFIG_TYPESCRIPT), babel(CONFIG_BABEL)],
+}
+
+const PACKAGE_REACT = (process.env.BUILD !== 'development' ||
+  process.env.PACKAGE === 'react') && {
+  input: 'src/react/index.ts',
+  output: [
+    {
+      file: 'react.js',
+      format: 'cjs',
+      globals: { react: 'React' },
+      strict: true,
+      sourcemap: true,
+    },
+    {
+      file: 'react.esm.js',
+      format: 'esm',
+      globals: { react: 'React' },
+      strict: true,
+      sourcemap: true,
+    },
+  ],
+  plugins: [resolve(), typescript(CONFIG_TYPESCRIPT), babel(CONFIG_BABEL)],
+  external: ['react'],
+}
+
+const PACKAGES = [PACKAGE_VANILLA, PACKAGE_REACT].filter(Boolean)
+export default PACKAGES
