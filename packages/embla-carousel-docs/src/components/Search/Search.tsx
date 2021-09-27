e7e19f0c698b58715db5742698444c077577284e
davidcetinkaya <david.o.cetinkaya@gmail.com>
2021-09-27 21:45:22 +0200

    Add defer to Algolia script

--- packages/embla-carousel-docs/src/components/Search/Search.tsx
+++ packages/embla-carousel-docs/src/components/Search/Search.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import FocusTrap from 'focus-trap-react'
 import styled, { css } from 'styled-components'
 import { useEventListener, useSearch } from 'hooks'
-import { breakpoints, LAYERS } from 'consts'
+import { breakpoints, LAYERS, URLS } from 'consts'
 import { Input } from './Input'
+import { isBrowser } from 'utils'
 
 export const SEARCH_ID = 'site_search'
 const DIALOG_MAX_WIDTH = '56rem'
@@ -44,6 +45,7 @@ const SearchDialog = styled.div`
 `
 
 export const Search = () => {
+  const [scriptLoaded, setScriptLoaded] = useState(false)
   const { isOpen, closeSearch } = useSearch()
 
   const onKeyUp = useCallback(
@@ -58,6 +60,16 @@ export const Search = () => {
     listener: onKeyUp,
   })
 
+  useEffect(() => {
+    if (!isBrowser) return
+    const script = document.createElement('script')
+    script.src = URLS.ALGOLIA_DOCSEARCH
+    script.async = true
+    script.defer = true
+    document.body.appendChild(script)
+    script.addEventListener('load', () => setScriptLoaded(true))
+  }, [])
+
   return (
     <FocusTrap active={isOpen}>
       <Wrapper
@@ -68,9 +80,7 @@ export const Search = () => {
         aria-label="Search Dialog"
       >
         <Overlay onPointerUp={closeSearch} />
-        <SearchDialog>
-          <Input />
-        </SearchDialog>
+        <SearchDialog>{scriptLoaded && <Input />}</SearchDialog>
       </Wrapper>
     </FocusTrap>
   )
