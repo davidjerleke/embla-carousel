3f0a39c99ef3cabe9c9a7f68882863b29ecf3fcd
David Jerleke <david.jerleke@gmail.com>
2023-03-13 22:06:07 +0100

    Disable italic font because it's not used now

--- packages/embla-carousel-docs/src/consts/fontFace.ts
+++ packages/embla-carousel-docs/src/consts/fontFace.ts
@@ -1,6 +1,6 @@
 import { css } from 'styled-components'
-import { INTER_ITALIC_VARIABLE_BASE_64 } from 'assets/fonts/inter-italic-var-base64'
 import { INTER_ROMAN_VARIABLE_BASE_64 } from 'assets/fonts/inter-roman-var-base64'
+// import { INTER_ITALIC_VARIABLE_BASE_64 } from 'assets/fonts/inter-italic-var-base64'
 
 export const FONT_FAMILY = `'Inter var'`
 const FONT_WEIGHT_RANGE = '400 900'
@@ -17,16 +17,16 @@ export const FONT_FACE_STYLES = css`
         url(${INTER_ROMAN_VARIABLE_BASE_64}) format('woff2-variations'),
         url(${INTER_ROMAN_VARIABLE_BASE_64}) format('woff2');
     }
-
-    @font-face {
-      font-family: ${FONT_FAMILY};
-      font-weight: ${FONT_WEIGHT_RANGE};
-      font-style: italic;
-      font-named-instance: 'Italic';
-      src: url(${INTER_ITALIC_VARIABLE_BASE_64})
-          format('woff2 supports variations(gvar)'),
-        url(${INTER_ITALIC_VARIABLE_BASE_64}) format('woff2-variations'),
-        url(${INTER_ITALIC_VARIABLE_BASE_64}) format('woff2');
-    }
   }
 `
+
+// @font-face {
+//   font-family: ${FONT_FAMILY};
+//   font-weight: ${FONT_WEIGHT_RANGE};
+//   font-style: italic;
+//   font-named-instance: 'Italic';
+//   src: url(${INTER_ITALIC_VARIABLE_BASE_64})
+//       format('woff2 supports variations(gvar)'),
+//     url(${INTER_ITALIC_VARIABLE_BASE_64}) format('woff2-variations'),
+//     url(${INTER_ITALIC_VARIABLE_BASE_64}) format('woff2');
+// }
