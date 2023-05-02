127d8e1198d7fd78592dbad6f1c85acf06fc3f16
David Jerleke <david.jerleke@gmail.com>
2023-05-02 22:03:15 +0200

    Fix Autoheight error

--- packages/embla-carousel-auto-height/src/components/AutoHeight.ts
+++ packages/embla-carousel-auto-height/src/components/AutoHeight.ts
@@ -54,7 +54,7 @@ function AutoHeight(userOptions: AutoHeightOptionsType = {}): AutoHeightType {
 
   function highestInView(): number {
     const { slidesInView, target } = carousel.internalEngine()
-    const inViewIndexes = slidesInView.check(target.get(), slideBounds)
+    const inViewIndexes = slidesInView.check(target.value, slideBounds)
     const heights = inViewIndexes.map((index) => slideHeights[index])
     return heights.reduce((a, b) => Math.max(a, b), 0)
   }
