9232b87977a039a69bf3d537b088641df4d4ac9e
David Jerleke <david.jerleke@gmail.com>
2023-08-22 10:11:46 +0200

    Build docs for v8.0.0-rc12.

---
+++ docs/6635-ccca5a91a90305d4f60e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkembla_carousel_docs=self.webpackChunkembla_carousel_docs||[]).push([[6635],{6635:function(e,a,n){n.r(a),a.default="import React from 'react'\n\nconst Header: React.FC = () => (\n  <header>\n    <h1 className=\"sandbox__header\">__replace_sandbox_title__</h1>\n  </header>\n)\n\nexport default Header\n"}}]);
+//# sourceMappingURL=6635-ccca5a91a90305d4f60e.js.map
