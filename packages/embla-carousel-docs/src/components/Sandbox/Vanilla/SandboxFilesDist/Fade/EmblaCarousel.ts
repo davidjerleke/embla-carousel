7cc1aee2506e7ec4ffeb4eaadb94b7259b286c1c
David Jerleke <david.jerleke@gmail.com>
2024-05-17 21:15:45 +0200

    Build docs with #878.

---
+++ packages/embla-carousel-docs/src/components/Sandbox/Vanilla/SandboxFilesDist/Fade/EmblaCarousel.ts
@@ -0,0 +1,31 @@
+import EmblaCarousel, { EmblaOptionsType } from 'embla-carousel'
+import Fade from 'embla-carousel-fade'
+import { addDotBtnsAndClickHandlers } from '../EmblaCarouselDotButton'
+import { addPrevNextBtnsClickHandlers } from '../EmblaCarouselArrowButtons'
+import '../css/base.css'
+import '../css/sandbox.css'
+import '../css/embla.css'
+
+const OPTIONS: EmblaOptionsType = {}
+
+const emblaNode = <HTMLElement>document.querySelector('.embla')
+const viewportNode = <HTMLElement>emblaNode.querySelector('.embla__viewport')
+const prevBtn = <HTMLElement>emblaNode.querySelector('.embla__button--prev')
+const nextBtn = <HTMLElement>emblaNode.querySelector('.embla__button--next')
+const dotsNode = <HTMLElement>document.querySelector('.embla__dots')
+
+const emblaApi = EmblaCarousel(viewportNode, OPTIONS, [Fade()])
+
+const removePrevNextBtnsClickHandlers = addPrevNextBtnsClickHandlers(
+  emblaApi,
+  prevBtn,
+  nextBtn
+)
+const removeDotBtnsAndClickHandlers = addDotBtnsAndClickHandlers(
+  emblaApi,
+  dotsNode
+)
+
+emblaApi
+  .on('destroy', removePrevNextBtnsClickHandlers)
+  .on('destroy', removeDotBtnsAndClickHandlers)
