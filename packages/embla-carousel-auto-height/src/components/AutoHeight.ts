95a0ac5cb6bd1629a1479b285a73ae22c778b315
Lucas Bordignon <bordi.lucas@gmail.com>
2024-06-10 16:22:19 -0300

    @embla-carousel-auto-height | Start using ResizeObserver for auth height calculations

--- packages/embla-carousel-auto-height/src/components/AutoHeight.ts
+++ packages/embla-carousel-auto-height/src/components/AutoHeight.ts
@@ -14,6 +14,7 @@ export type AutoHeightOptionsType = AutoHeightType['options']
 
 function AutoHeight(userOptions: AutoHeightOptionsType = {}): AutoHeightType {
   let emblaApi: EmblaCarouselType
+  let resizeObserver: ResizeObserver
   let slideHeights: number[] = []
   const heightEvents: EmblaEventType[] = ['select', 'slideFocus']
 
@@ -21,22 +22,33 @@ function AutoHeight(userOptions: AutoHeightOptionsType = {}): AutoHeightType {
     emblaApi = emblaApiInstance
 
     const {
-      options: { axis },
-      slideRects
+      options: { axis }
     } = emblaApi.internalEngine()
 
     if (axis === 'y') return
 
-    slideHeights = slideRects.map((slideRect) => slideRect.height)
+    resizeObserver = new ResizeObserver(synchronizeSlideHeights)
+
+    emblaApi.slideNodes().forEach((node: HTMLElement) => {
+      resizeObserver.observe(node)
+    })
 
     heightEvents.forEach((evt) => emblaApi.on(evt, setContainerHeight))
-    setContainerHeight()
+
+    synchronizeSlideHeights()
   }
 
   function destroy(): void {
+    emblaApi.slideNodes().forEach((node: HTMLElement) => {
+      resizeObserver.unobserve(node)
+    })
+
     heightEvents.forEach((evt) => emblaApi.off(evt, setContainerHeight))
+
     const container = emblaApi.containerNode()
+
     container.style.height = ''
+
     if (!container.getAttribute('style')) container.removeAttribute('style')
   }
 
@@ -51,6 +63,14 @@ function AutoHeight(userOptions: AutoHeightOptionsType = {}): AutoHeightType {
 
   function setContainerHeight(): void {
     emblaApi.containerNode().style.height = `${highestInView()}px`
+  }
+
+  function synchronizeSlideHeights(): void {
+    const slideNodes = emblaApi.slideNodes()
+
+    slideHeights = slideNodes.map((node: HTMLElement) => node.offsetHeight)
+
+    setContainerHeight()
   }
 
   const self: AutoHeightType = {
