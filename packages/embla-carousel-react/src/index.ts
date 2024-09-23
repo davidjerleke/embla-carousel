ce0bb3f7a7b3fcbc2b43532b589337639bc49b7e
Federico <16429579+rawnly@users.noreply.github.com>
2024-09-23 17:38:39 +0200

    re-export `EmblaCarouselType`

--- packages/embla-carousel-react/src/index.ts
+++ packages/embla-carousel-react/src/index.ts
@@ -2,4 +2,7 @@ export {
   UseEmblaCarouselType,
   EmblaViewportRefType
 } from './components/useEmblaCarousel'
+
 export { default } from './components/useEmblaCarousel'
+
+export { EmblaCarouselType } from 'embla-carousel'
