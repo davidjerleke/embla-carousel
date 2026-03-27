bdf741c77cbe25049d436d417af41aeebfe02b23
David Jerleke <david.jerleke@gmail.com>
2026-03-27 12:56:08 +0100

    Repo - Migrate to flat eslint configs

---
+++ packages/embla-carousel-vue/eslint.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier/recommended'
+
+export default defineConfig(
+  {
+    ignores: [
+      'docs/**',
+      'package.json',
+      'package-lock.json',
+      'yarn.lock',
+      'node_modules/**'
+    ]
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierPlugin,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      'no-debugger': 2,
+      'no-console': 2,
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off'
+    }
+  }
+)
